817830b9c2226485d254ca2dad3452a05cb65b5b
Ayush Kumar Karmi <ayushkumarkarmi30@gmail.com>
2025-02-27 00:12:08 +0530

    Create file

---
+++ 712-MinimumASCIIDeleteSumforTwoStrings/712-MinimumASCIIDeleteSumforTwoStrings.cpp
@@ -0,0 +1,28 @@
+int solveMemo(string &s1,string &s2,int i,int j,vector<vector<int>>&dp){
+        if(i==s1.length() || j==s2.length()){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int ans=0;
+        if(s1[i]==s2[j]){
+            ans=s1[i]+solveMemo(s1,s2,i+1,j+1,dp);
+        }else{
+            ans=max(solveMemo(s1,s2,i+1,j,dp),solveMemo(s1,s2,i,j+1,dp));
+        }
+        return dp[i][j]=ans;
+    }
+	int minimumDeleteSum(string s1, string s2) {
+        int n=s1.length();
+        int m=s2.length();
+        int sum=0;
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        for(int i=0;i<n;i++){
+            sum+=s1[i];
+        }
+        for(int i=0;i<m;i++){
+            sum+=s2[i];
+        }
+        return sum-(2*(solveMemo(s1,s2,0,0,dp)));
+    }
