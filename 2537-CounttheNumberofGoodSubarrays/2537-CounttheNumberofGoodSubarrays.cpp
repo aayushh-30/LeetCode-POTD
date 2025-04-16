107d73657529f98485ae889e9f9513bca6f9f088
Ayush Kumar Karmi <ayushkumarkarmi30@gmail.com>
2025-04-16 16:19:03 +0530

    Create file

---
+++ 2537-CounttheNumberofGoodSubarrays/2537-CounttheNumberofGoodSubarrays.cpp
@@ -0,0 +1,23 @@
+// Last updated: 4/16/2025, 4:19:01 PM
+class Solution {
+public:
+    long long countGood(vector<int>& nums, int k) {
+        unordered_map<int, int> freq;
+        int left = 0;
+        long long pairCount = 0, goodSubarrays = 0;
+
+        for (int right = 0; right < nums.size(); ++right) {
+            pairCount += freq[nums[right]];
+            freq[nums[right]]++;
+
+            while (pairCount >= k) {
+                goodSubarrays += nums.size() - right;
+                freq[nums[left]]--;
+                pairCount -= freq[nums[left]];
+                left++;
+            }
+        }
+
+        return goodSubarrays;
+    }
+};
