c2693354f7ff4d17efb518cd9343e22af9b6cac3
Ayush Kumar Karmi <ayushkumarkarmi30@gmail.com>
2025-10-26 00:25:25 +0530

    Create 3724-MinimumOperationstoTransformArray

---
+++ 3724-MinimumOperationstoTransformArray/3724-MinimumOperationstoTransformArray.cpp
@@ -0,0 +1,15 @@
+// Last updated: 10/26/2025, 12:25:24 AM
+    long long minOperations(vector<int>& A, vector<int>& B) {
+        int v = B.back(), last = 1e9;
+        long long res = 0;
+        for (int i = 0; i < A.size(); ++i) {
+            int a = A[i], b = B[i];
+            res += abs(a - b);
+            if ((a <= v && v <= b) || (b <= v && v <= a))
+                last = 0;
+            else {
+                last = min(last, min(abs(a - v), abs(b - v)));
+            }
+        }
+        return res + last + 1;
+    }
