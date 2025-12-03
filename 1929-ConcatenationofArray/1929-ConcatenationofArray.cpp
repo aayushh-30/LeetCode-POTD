c65ca9e836af3ca1fb95fc889234c2eddb55a77a
Ayush Kumar Karmi <ayushkumarkarmi30@gmail.com>
2025-12-03 23:34:54 +0530

    Create 1929-ConcatenationofArray

---
+++ 1929-ConcatenationofArray/1929-ConcatenationofArray.cpp
@@ -0,0 +1,21 @@
+// Last updated: 12/3/2025, 11:34:52 PM
+1class Solution {
+2public:
+3    int findMaxConsecutiveOnes(vector<int>& nums) {
+4        int len = 0;
+5        for(int i=0;i<nums.size();i++){
+6            int j = i;
+7            while(j<nums.size() && nums[j] == 1){
+8                j++;
+9                len = max(len,j-i);
+10            }
+11            i = j;
+12            
+13
+14        }
+15
+16        return len;
+17
+18        
+19    }
+20};
