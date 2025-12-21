1a36c5ff642d79ec75a7990974cbfde7bcf35beb
Ayush Kumar Karmi <ayushkumarkarmi30@gmail.com>
2025-12-21 14:09:12 +0530

    File updated at 12/21/2025, 2:09:10 PM

--- 3783-MirrorDistanceofanInteger/3783-MirrorDistanceofanInteger.cpp
+++ 3783-MirrorDistanceofanInteger/3783-MirrorDistanceofanInteger.cpp
@@ -1,17 +1,23 @@
-// Last updated: 12/21/2025, 1:37:15 PM
-1class Solution {
-2    int reverse(int n){
-3        int rev = 0;
-4        while(n > 0){
-5            int rem = n % 10;
-6            rev = rev*10 + rem;
-7            n /= 10;
+// Last updated: 12/21/2025, 2:09:10 PM
+1#define ll long long
+2class Solution {
+3public:
+4    long long minCost(string s, vector<int>& cost) {
+5        unordered_map<char,ll> mp;
+6        for(int i=0;i<s.length();i++){
+7            mp[s[i]] += cost[i];
 8        }
-9        return rev;
-10    }
-11public:
-12    int mirrorDistance(int n) {
-13        return abs(n-reverse(n));
-14        
-15    }
-16};
+9        if(mp.size() == 1) return 0;
+10        ll total = accumulate(cost.begin(),cost.end(),0LL);
+11
+12        ll ans = total;
+13        for(auto it:mp){
+14            ll cost = it.second;
+15            ans = min(ans,total-cost);
+16
+17        }
+18        return ans;
+19        
+20        
+21    }
+22};
