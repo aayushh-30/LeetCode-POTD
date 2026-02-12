f34abce0d691d494e78c928a1302b7ea647c1412
Ayush Kumar Karmi <ayushkumarkarmi30@gmail.com>
2026-02-12 10:47:01 +0530

    File updated at 2/12/2026, 10:47:00 AM

--- 3713-LongestBalancedSubstringI/3713-LongestBalancedSubstringI.cpp
+++ 3713-LongestBalancedSubstringI/3713-LongestBalancedSubstringI.cpp
@@ -1,36 +1,36 @@
-// Last updated: 10/12/2025, 6:53:18 PM
-class Solution {
-    bool fun(vector<int> &freq){
-        int lastSeen = 0;
-        for(int i=0;i<26;i++){
-            if(freq[i] == 0) continue;
-            if(lastSeen != 0 && freq[i] != lastSeen) return false;
-            lastSeen = freq[i];
-        }
-        return true;
-
-    }
-public:
-    int longestBalanced(string s) {
-        vector<int> freq(26,0);
-        int n = s.length();
-        if(n == 1) return 1;
-        int ans = 0;
-        for(int i=0;i<n;i++){
-            string sub = "";
-            sub += s[i];
-            freq[s[i]-'a']++;
-            for(int j=i+1;j<n;j++){
-                sub += s[j];
-                freq[s[j]-'a']++;
-                if(fun(freq)){
-                    ans = max(ans,j - i + 1);
-                }
-            }
-            freq.clear();
-            freq.resize(26,0);
-        }
-        return ans;
-        
-    }
-};
+// Last updated: 2/12/2026, 10:47:00 AM
+1class Solution {
+2    bool fun(vector<int> &freq){
+3        int lastSeen = 0;
+4        for(int i=0;i<26;i++){
+5            if(freq[i] == 0) continue;
+6            if(lastSeen != 0 && freq[i] != lastSeen) return false;
+7            lastSeen = freq[i];
+8        }
+9        return true;
+10
+11    }
+12public:
+13    int longestBalanced(string s) {
+14        vector<int> freq(26,0);
+15        int n = s.length();
+16        if(n == 1) return 1;
+17        int ans = 0;
+18        for(int i=0;i<n;i++){
+19            string sub = "";
+20            sub += s[i];
+21            freq[s[i]-'a']++;
+22            for(int j=i+1;j<n;j++){
+23                sub += s[j];
+24                freq[s[j]-'a']++;
+25                if(fun(freq)){
+26                    ans = max(ans,j - i + 1);
+27                }
+28            }
+29            freq.clear();
+30            freq.resize(26,0);
+31        }
+32        return ans;
+33        
+34    }
+35};
