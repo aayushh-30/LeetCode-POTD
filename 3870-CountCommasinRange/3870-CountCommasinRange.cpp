1808d8bb9886cc813656cf6868f6ea3f78eb1652
Ayush Kumar Karmi <ayushkumarkarmi30@gmail.com>
2026-03-15 23:04:28 +0530

    File updated at 3/15/2026, 11:04:26 PM

--- 3870-CountCommasinRange/3870-CountCommasinRange.cpp
+++ 3870-CountCommasinRange/3870-CountCommasinRange.cpp
@@ -1,10 +1,37 @@
-// Last updated: 3/15/2026, 11:01:03 PM
-1class Solution {
-2public:
-3    int countCommas(int n) {
-4        if(n<1000) return 0;
-5        return n-1000+1;
-6
-7        
-8    }
-9};
+// Last updated: 3/15/2026, 11:04:26 PM
+    long comma = 1;
+
+    long ans = 0;
+
+    for(int i=0;i<arr.length;i++){
+
+        long left = arr[i][0];
+        long right = arr[i][1];
+
+        long diff = 0 , mul = 0;
+
+        if(n>=left && n<=right){
+
+             diff = n - left;
+            diff++;
+
+              mul = diff * comma;
+            ans+=mul;
+
+        }else if(n>right){
+             diff = right - left;
+            diff++;
+
+             mul = diff * comma;
+            ans+=mul;
+        }
+        comma++;
+    }
+    
+    return ans;
+} 
+// 1,002 , 1,000,023   , 12,232,323 ,  1,293,929,394
+// 1,000 == 999,999
+// 1,000,000 == 999,999,999
+// 1,000,000,000 == 999,999,999,999
+// 1,000,000,000,000 == 999,999,999,999,999    1,000,000,000,000,000 
