0b0a4de359fbb68fa99b1fccf4a26c780d2df7ba
Ayush Kumar Karmi <ayushkumarkarmi30@gmail.com>
2026-03-17 12:54:33 +0530

    File updated at 3/17/2026, 12:54:32 PM

--- 1502-CanMakeArithmeticProgressionFromSequence/1502-CanMakeArithmeticProgressionFromSequence.cpp
+++ 1502-CanMakeArithmeticProgressionFromSequence/1502-CanMakeArithmeticProgressionFromSequence.cpp
@@ -1,15 +1,16 @@
-// Last updated: 3/17/2026, 12:43:00 PM
+// Last updated: 3/17/2026, 12:54:32 PM
 1class Solution {
 2public:
-3    bool canMakeArithmeticProgression(vector<int>& arr) {
-4        sort(arr.begin(),arr.end());
-5        int n = arr.size();
-6        int diff = arr[1] - arr[0];
-7        for(int i=2;i<n;i++){
-8            if(arr[i]-arr[i-1] != diff) return false;
-9        }
-10
-11        return true;
-12        
-13    }
-14};
+3    bool isPalindrome(int x) {
+4        if(x<0) return false;
+5        int temp = x;
+6        long long n = 0;
+7        while(temp){
+8            n = (n * 10) + (temp % 10);
+9            temp /= 10;
+10        }
+11
+12        return x == n;
+13        
+14    }
+15};
