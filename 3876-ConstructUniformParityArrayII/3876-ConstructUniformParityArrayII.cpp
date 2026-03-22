fe0aa38c380e0ed7651d5ff9a63d0e91d362c201
Ayush Kumar Karmi <ayushkumarkarmi30@gmail.com>
2026-03-22 16:31:14 +0530

    Create 3876-ConstructUniformParityArrayII

---
+++ 3876-ConstructUniformParityArrayII/3876-ConstructUniformParityArrayII.cpp
@@ -0,0 +1,20 @@
+// Last updated: 3/22/2026, 4:31:12 PM
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int mini=INT_MAX;
+        bool odd=false;
+        for(auto t:nums1){
+            
+            if(t%2){
+                odd=true;
+                mini=min(mini,t);
+            }
+        }
+        if(!odd) return true;
+        for(int t:nums1){
+            if(t%2==0 && mini>t) return false;
+        }
+        return true;
+    }
+};
