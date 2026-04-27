f919fb9ab0ccb22c53886c878175a364359fdff6
Ayush Kumar Karmi <ayushkumarkarmi30@gmail.com>
2026-04-28 00:17:04 +0530

    Create 3908-ValidDigitNumber

---
+++ 3908-ValidDigitNumber/3908-ValidDigitNumber.cpp
@@ -0,0 +1,22 @@
+// Last updated: 4/28/2026, 12:17:04 AM
+1class Solution {
+2    bool cntX(int n, int x, int& ld) {
+3        int cnt = 0;
+4        while (n > 0) {
+5            ld = n % 10;
+6            if (ld == x) cnt++;
+7            n /= 10;
+8        }
+9        return cnt >= 1;
+10    }
+11
+12public:
+13    bool validDigit(int n, int x) {
+14        int ld = 1e9;
+15        
+16        bool hd = cntX(n,x,ld);
+17
+18        cout<< hd<<endl;
+19        return hd && (ld != x);
+20    }
+21};
